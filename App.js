f9c82a5d4ad4875df215ab7d00f804e45d2a2368
Raisa <raisaerlich@gmail.com>
2022-10-28 13:29:42 -0300

    fixed default text

--- App.js
+++ App.js
@@ -1,31 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-import {
-  Button,
-  StyleSheet,
-  View,
-  Image,
-  ScrollView,
-  SafeAreaView,
-} from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 
